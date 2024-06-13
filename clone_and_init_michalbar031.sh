e611d1d4d7e95902eda0e329f0cf5af32ddf4e69
michal barsht <81368958+michalbar031@users.noreply.github.com>
2024-06-13 15:35:37 +0300

    Add files via upload

---
+++ clone_and_init_michalbar031.sh
@@ -0,0 +1,9 @@
+# cloning your code from github:
+git clone https://github.com/michalbar031/NLP2024_project.git
+
+cd NLP2024_project
+
+conda env create -f requirements.yml
+
+# Your main sweep:
+python final_sweep_michalbar031.py
